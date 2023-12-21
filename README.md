e75b0c2de10fe45de2a960fc9527cb3ad0cf08f9
Michael J. Fitzgerald <michael.fitzgerald.1406@gmail.com>
2023-12-21 15:18:37 -0600

    experimented with Korean and Chinese

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # MagniText
 A streamlined website designed to responsively maximize the font size of any input message, ensuring optimal utilization of the entire viewport. This tool is especially useful for displaying messages on a phone, enabling easy readability.
 
+## Change Log
+- 2023/12/xx
+    - added support for Korean
+    - added support for Chinese
+
 ## Libraries
 - [Tailwind CSS](https://tailwindcss.com/)
 - [Font Awesome](https://fontawesome.com/)
--- app.js
+++ app.js
@@ -23,7 +23,7 @@ buttonClearText.addEventListener('click', function(){
 
 function maxText(){
     let numberOfCharacters = textArea.value.length
-    if (numberOfCharacters < 6) return
+    if (numberOfCharacters < 5) return
     
     let fontModifier = 1.10 // Default for all devices
     if (isMobileDevice() && isLandscape()) fontModifier = 1.05
@@ -38,31 +38,27 @@ function maxText(){
     let areaPerCharacter = area / numberOfCharacters
     let approximateFontSize = Math.sqrt(areaPerCharacter) * fontModifier
     let approximateFontWidth = approximateFontSize * 0.6
-    console.log("PARAMS", {
-        WIDTH: width, 
-        HEIGHT: height, 
-        AREA: area, 
-        NUMBER_OF_CHARS: numberOfCharacters, 
-        AREA_PER_CHAR: areaPerCharacter, 
-        APPROX_FONT_WIDTH: approximateFontWidth,
-        APPROX_FONT_SIZE: approximateFontSize,
-        FONT_MODIFIER: fontModifier
-    })
+    // console.log("PARAMS", {
+    //     WIDTH: width, 
+    //     HEIGHT: height, 
+    //     AREA: area, 
+    //     NUMBER_OF_CHARS: numberOfCharacters, 
+    //     AREA_PER_CHAR: areaPerCharacter, 
+    //     APPROX_FONT_WIDTH: approximateFontWidth,
+    //     APPROX_FONT_SIZE: approximateFontSize,
+    //     FONT_MODIFIER: fontModifier
+    // })
 
     let fontSize = approximateFontSize // TODO: Need to ensure words can't be too long for the width
     const lineHeight = fontSize + 6
-    console.log("FONT", {SIZE: fontSize, LINE_HEIGHT: lineHeight})
 
     textArea.style.fontSize = `${fontSize}px`
     textArea.style.lineHeight = `${lineHeight}px`
 }
 
 function isMobileDevice() {
-    // return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
-    // return navigator.userAgentData && navigator.userAgentData.mobile;
     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
 };
 function isLandscape() {
-    // return window.innerWidth > window.innerHeight;
     return window.matchMedia("(orientation: landscape)").matches;
 }
